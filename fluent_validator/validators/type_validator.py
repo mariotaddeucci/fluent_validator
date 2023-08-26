c20e9253c4721d3ecafde849136e64a9303831d3
Mario Taddeucci <mariotaddeucci@gmx.com>
2023-08-26 20:48:15 -0300

    Add python docstring

--- fluent_validator/validators/type_validator.py
+++ fluent_validator/validators/type_validator.py
@@ -4,20 +4,42 @@ from fluent_validator.validators.base_validator import BaseValidator
 
 
 class TypeValidator(BaseValidator):
-    def _is_instance(self, *args) -> bool:
+    def _is_instance(self, *args):
+        """
+        Check if the object is an instance of one or more specified types.
+
+        Args:
+            *args: One or more types to check against.
+        """
+
         return isinstance(self.obj, args)
 
-    def _is_callable(self) -> bool:
+    def _is_callable(self):
+        """
+        Check if the object is callable (e.g., a function or method).
+        """
         return callable(self.obj)
 
-    def _is_iterable(self) -> bool:
+    def _is_iterable(self):
+        """
+        Check if the object is iterable.
+        """
         return self._is_instance(Iterable)
 
-    def _is_string(self) -> bool:
+    def _is_string(self):
+        """
+        Check if the object is a string.
+        """
         return self._is_instance(str)
 
-    def _is_number(self) -> bool:
+    def _is_number(self):
+        """
+        Check if the object is a number (int or float).
+        """
         return self._is_instance(int, float)
 
-    def _is_bool(self) -> bool:
+    def _is_bool(self):
+        """
+        Check if the object is a boolean.
+        """
         return self._is_instance(bool)
--- fluent_validator/validators/value_validator.py
+++ fluent_validator/validators/value_validator.py
@@ -2,50 +2,137 @@ from fluent_validator.validators.type_validator import TypeValidator
 
 
 class ValueValidator(TypeValidator):
-    def _equal(self, value) -> bool:
+    def _equal(self, value):
+        """
+        Check if the object is equal to the specified value.
+
+        Args:
+            value: The value to compare with.
+        """
         return self.obj == value
 
-    def _is_in(self, *args) -> bool:
+    def _is_in(self, *args):
+        """
+        Check if the object is in a collection of values.
+
+        Args:
+            *args: Values to check for containment.
+        """
         return self.obj in args
 
-    def _greater_than(self, value) -> bool:
+    def _greater_than(self, value):
+        """
+        Check if the object is greater than the specified value.
+
+        Args:
+            value: The value to compare with.
+        """
         return self.obj > value
 
-    def _less_than(self, value) -> bool:
+    def _less_than(self, value):
+        """
+        Check if the object is less than the specified value.
+
+        Args:
+            value: The value to compare with.
+        """
         return self.obj < value
 
-    def _greater_or_equal_than(self, value) -> bool:
+    def _greater_or_equal_than(self, value):
+        """
+        Check if the object is greater than or equal to the specified value.
+
+        Args:
+            value: The value to compare with.
+        """
         return self.obj >= value
 
-    def _less_or_equal_than(self, value) -> bool:
+    def _less_or_equal_than(self, value):
+        """
+        Check if the object is less than or equal to the specified value.
+
+        Args:
+            value: The value to compare with.
+        """
         return self.obj <= value
 
-    def _min(self, value) -> bool:
+    def _min(self, value):
+        """
+        Check if the object is greater than or equal to the specified minimum value.
+
+        Args:
+            value: The minimum value to compare with.
+        """
         return self._greater_or_equal_than(value)
 
-    def _max(self, value) -> bool:
+    def _max(self, value):
+        """
+        Check if the object is less than or equal to the specified maximum value.
+
+        Args:
+            value: The maximum value to compare with.
+        """
         return self._less_or_equal_than(value)
 
-    def _between(self, min, max) -> bool:
+    def _between(self, min, max):
+        """
+        Check if the object is within the specified range.
+
+        Args:
+            min: The minimum value of the range.
+            max: The maximum value of the range.
+        """
         return self._min(min) and self._max(max)
 
-    def _is_true(self) -> bool:
+    def _is_true(self):
+        """
+        Check if the object is a boolean and has a value of True.
+        """
         return self._is_bool() and self.obj is True
 
-    def _is_false(self) -> bool:
+    def _is_false(self):
+        """
+        Check if the object is a boolean and has a value of False.
+        """
         return self._is_bool() and self.obj is False
 
-    def _contains_at_least(self, value) -> bool:
+    def _contains_at_least(self, value):
+        """
+        Check if the object (assumed to be iterable) contains at least the specified number of elements.
+
+        Args:
+            value: The minimum number of elements to check for.
+        """
         return len(self.obj) >= value
 
-    def _contains_at_most(self, value) -> bool:
+    def _contains_at_most(self, value):
+        """
+        Check if the object (assumed to be iterable) contains at most the specified number of elements.
+
+        Args:
+            value: The maximum number of elements to check for.
+        """
         return len(self.obj) <= value
 
-    def _contains_exactly(self, value) -> bool:
+    def _contains_exactly(self, value):
+        """
+        Check if the object (assumed to be iterable) contains exactly the specified number of elements.
+
+        Args:
+            value: The exact number of elements to check for.
+        """
         return len(self.obj) == value
 
-    def _is_none(self) -> bool:
+    def _is_none(self):
+        """
+        Check if the object is None.
+        """
         return self.obj is None
 
-    def _has_unique_values(self) -> bool:
+    def _has_unique_values(self):
+        """
+        Check if the object (assumed to be iterable) contains unique values.
+
+        Note: This function assumes that the object's elements are hashable.
+        """
         return len(self.obj) == len(set(self.obj))
