abe834a11cba76a6a8baa15278b68a83d5176a56
Mario Taddeucci <mariotaddeucci@gmx.com>
2023-08-26 22:04:55 -0300

    Add optional dependencies

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url="https://github.com/mariotaddeucci/fluent_validator",
     long_description_content_type="text/markdown",
     long_description=long_description,
-    packages=find_packages(exclude=["tests", "tests.*", "docs", "docs.*", "scripts"]),
+    packages=["fluent_validator", "fluent_validator.validators"],
     install_requires=[],
     keywords=["validator", "check", "fluent", "assert"],
     classifiers=[
