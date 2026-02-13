83b990fd37bddf8d496fc7e6f40dddc43c9020c4
Mario Taddeucci <mariotaddeucci@gmx.com>
2026-02-13 16:48:06 -0300

    Adjust Validator

--- src/fluent_validator/__init__.py
+++ src/fluent_validator/__init__.py
@@ -1,10 +1,10 @@
 """Public exports for fluent_validator package.
 
-Expose ValidationError, ValidatorBuilder and ValidatorSpec.
+Expose ValidationError, Validator and ValidatorSpec.
 """
 
 from fluent_validator.exceptions import ValidationError
-from fluent_validator.validator_builder import ValidatorBuilder
+from fluent_validator.validator_builder import ValidatorBuilder as Validator
 from fluent_validator.validator_spec import ValidatorSpec
 
-__all__ = ["ValidationError", "ValidatorBuilder", "ValidatorSpec"]
+__all__ = ["ValidationError", "Validator", "ValidatorSpec"]
